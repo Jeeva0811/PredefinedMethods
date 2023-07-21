d2e503ff0ab815b568c0fe250aeef2adc72e09ce
jeevar <jeevaselvam619@gmail.com>
2023-07-21 19:19:55 +0530

    pre-defined method

---
+++ NumberSystem.java
@@ -0,0 +1,9 @@
+public class NumberSystem
+{
+public static void main(String args[])
+{
+System.out.println(Integer.toBinaryString(32));
+System.out.println(Integer.toOctalString(32));
+System.out.println(Integer.toHexString(32));
+}
+}
